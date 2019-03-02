107550629a9ba9470a3573c2757ecdb565b7cb60
McPollux <enox444@hotmail.com>
2019-03-02 18:01:38 +0100

    Banco con neodatis funcional

--- ProyectoX_Clientes/src/logintemp/ControladorBanco.java
+++ ProyectoX_Clientes/src/logintemp/ControladorBanco.java
@@ -11,27 +11,37 @@ import Recursos.Inserciones;
 import Recursos.Windows;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
+
 import java.util.List;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
 import org.hibernate.Session;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import Objetos.*;
+
 import java.sql.Date;
+
 import Funciones.Validaciones;
 import com.jfoenix.controls.JFXTabPane;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.concurrent.atomic.AtomicReference;
+
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import org.neodatis.odb.ODB;
+import org.neodatis.odb.ODBFactory;
+import org.neodatis.odb.Objects;
+import org.neodatis.odb.core.query.criteria.Where;
+import org.neodatis.odb.impl.core.query.criteria.CriteriaQuery;
 
 /**
- *
  * @author horudi
  */
 public class ControladorBanco implements Initializable {
@@ -117,16 +127,54 @@ public class ControladorBanco implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         MoverVentanas(panel);
         vaciar(); //Vaciamos los label de la pestaña Cuentas
-        Session s = NewHibernateUtil.getSession();
-        Clientes clienteActual = (Clientes) s.get(Clientes.class, LoginTemp.getClienteActual().getId());
+
+        if (LoginTemp.bbdd == 0) {
+            Session s = NewHibernateUtil.getSession();
+            Clientes clienteActual = (Clientes) s.get(Clientes.class, LoginTemp.getClienteActual().getId());
+
+            cargarCombos(clienteActual);
+            //cmbCuenta.getSelectionModel().select(cmbCuenta2.getSelectionModel().getSelectedIndex()); //Seleccionamos el mismo que este abierto en la otra ventana
+            s.close();
+        } else {
+            ODB odb = ODBFactory.openClient("localhost", 8000, "proyectojjcv");
+            Clientes cliente = ODBCliente(odb);
+
+            cargarCombos(cliente);
+            odb.close();
+        }
+    }
+
+    private Clientes ODBCliente(ODB odb) {  // Metodo utilizado a lo largo de la clase para obtener el cliente actual de la BBDD
+        CriteriaQuery criteria = new CriteriaQuery(Clientes.class, Where.equal("dni", LoginTemp.getClienteActual().getDni()));
+        Objects<Clientes> cli = odb.getObjects(criteria);
+        return cli.getFirst();
+    }
+
+    private void cargarCombos(Clientes clienteActual) {  // Metodo utilizado a lo largo de la clase para cargar los combos con cuentas
         for (Cuentas cu : clienteActual.getCuentas()) {
             cmbCuenta.getItems().add(cu.getNumeroCuenta());
             cmbCuenta2.getItems().add(cu.getNumeroCuenta());
         }
-        //cmbCuenta.getSelectionModel().select(cmbCuenta2.getSelectionModel().getSelectedIndex()); //Seleccionamos el mismo que este abierto en la otra ventana
-        s.close();
     }
-        private void MoverVentanas(JFXTabPane root) {
+
+    public void vaciar() {
+        txtTitular.setText("");
+        txtTarjeta.setText("");
+        txtSaldo.setText("");
+        txtCVV.setText("");
+        txtCaducidad.setText("");
+    }
+
+    private void vaciarAlta() {
+        txtCuentaAlta.setText("");
+        txtClienteAlta.setText("");
+        txtSaldoAlta.setText("");
+        txtTarjetaAlta.setText("");
+        txtCVVAlta.setText("");
+        txtFechaCadAlta.setText("");
+    }
+
+    private void MoverVentanas(JFXTabPane root) {
 
         AtomicReference<Double> xOffset = new AtomicReference<>((double) 0);
         AtomicReference<Double> yOffset = new AtomicReference<>((double) 0);
@@ -145,14 +193,6 @@ public class ControladorBanco implements Initializable {
         });
     }
 
-    public void vaciar() {
-        txtTitular.setText("");
-        txtTarjeta.setText("");
-        txtSaldo.setText("");
-        txtCVV.setText("");
-        txtCaducidad.setText("");
-    }
-
     @FXML
     public void altaCuenta() throws ParseException {
         boolean b = true;
@@ -163,7 +203,6 @@ public class ControladorBanco implements Initializable {
             imgCaducidad.setVisible(true);
             lblAdvertencia.setText("Formato de fecha inválido");
         } else {
-
             imgCaducidad.setVisible(false);
         }
         //
@@ -202,35 +241,47 @@ public class ControladorBanco implements Initializable {
             imgCuenta.setVisible(false);
         }
         if (b) {
-            Session s = NewHibernateUtil.getSession();
-            Clientes clienteActual = (Clientes) s.get(Clientes.class, LoginTemp.getClienteActual().getId());
 
-            Tarjetas tarjeta = new Tarjetas(txtTarjetaAlta.getText(), sdf.parse(txtFechaCadAlta.getText()), txtCVVAlta.getText());
-            Cuentas cuenta = new Cuentas(txtCuentaAlta.getText(), Float.parseFloat(txtSaldoAlta.getText()), clienteActual, tarjeta);
+            if (LoginTemp.bbdd == 0) {
+                Session s = NewHibernateUtil.getSession();
+                Clientes clienteActual = (Clientes) s.get(Clientes.class, LoginTemp.getClienteActual().getId());
 
-            clienteActual.getCuentas().add(cuenta);
-            tarjeta.setCuenta(cuenta);
+                Tarjetas tarjeta = new Tarjetas(txtTarjetaAlta.getText(), sdf.parse(txtFechaCadAlta.getText()), txtCVVAlta.getText());
+                Cuentas cuenta = new Cuentas(txtCuentaAlta.getText(), Float.parseFloat(txtSaldoAlta.getText()), clienteActual, tarjeta);
 
-            s.beginTransaction();
-            s.save(tarjeta);
-            s.save(cuenta);
-            s.saveOrUpdate(clienteActual);
-            s.getTransaction().commit();
-            cmbCuenta.getItems().clear();
-            cmbCuenta2.getItems().clear();
-            Clientes cli = (Clientes) s.get(Clientes.class, LoginTemp.getClienteActual().getId());
-            for (Cuentas cu : cli.getCuentas()) {
-                cmbCuenta.getItems().add(cu.getNumeroCuenta());
-                cmbCuenta2.getItems().add(cu.getNumeroCuenta());
+                clienteActual.getCuentas().add(cuenta);
+                tarjeta.setCuenta(cuenta);
+
+                s.beginTransaction();
+                s.save(tarjeta);
+                s.save(cuenta);
+                s.saveOrUpdate(clienteActual);
+                s.getTransaction().commit();
+
+                cmbCuenta.getItems().clear();
+                cmbCuenta2.getItems().clear();
+                Clientes cli = (Clientes) s.get(Clientes.class, LoginTemp.getClienteActual().getId());
+                cargarCombos(cli);
+                s.close();
+            } else {
+                ODB odb = ODBFactory.openClient("localhost", 8000, "proyectojjcv");
+                Clientes cliente = ODBCliente(odb);
+
+                Tarjetas tarjeta = new Tarjetas(txtTarjetaAlta.getText(), sdf.parse(txtFechaCadAlta.getText()), txtCVVAlta.getText());
+                Cuentas cuenta = new Cuentas(txtCuentaAlta.getText(), Float.parseFloat(txtSaldoAlta.getText()), cliente, tarjeta);
+
+                cliente.getCuentas().add(cuenta);
+                tarjeta.setCuenta(cuenta);
+
+                cmbCuenta.getItems().clear();
+                cmbCuenta2.getItems().clear();
+                cargarCombos(cliente);
+
+                odb.store(cliente);
+                odb.commit();
+                odb.close();
             }
-            s.close();
-            txtCuentaAlta.setText("");
-            txtClienteAlta.setText("");
-            txtSaldoAlta.setText("");
-            txtTarjetaAlta.setText("");
-            txtCVVAlta.setText("");
-            txtFechaCadAlta.setText("");
-
+            vaciarAlta();
         }
     }
 
@@ -241,16 +292,28 @@ public class ControladorBanco implements Initializable {
 
             String numeroCuenta = cmbCuenta2.getSelectionModel().getSelectedItem();
 
-            Session s = NewHibernateUtil.getSession();
+            if (LoginTemp.bbdd == 0) {
+                Session s = NewHibernateUtil.getSession();
 
-            Cuentas c = (Cuentas) s.get(Cuentas.class, numeroCuenta);
+                Cuentas c = (Cuentas) s.get(Cuentas.class, numeroCuenta);
 
-            c.setSaldo(c.getSaldo() + Float.parseFloat(txtSaldoIng.getText()));
+                c.setSaldo(c.getSaldo() + Float.parseFloat(txtSaldoIng.getText()));
 
-            s.beginTransaction();
-            s.saveOrUpdate(c);
-            s.getTransaction().commit();
-            s.close();
+                s.beginTransaction();
+                s.saveOrUpdate(c);
+                s.getTransaction().commit();
+                s.close();
+            } else {
+                ODB odb = ODBFactory.openClient("localhost", 8000, "proyectojjcv");
+                CriteriaQuery criteria = new CriteriaQuery(Cuentas.class, Where.iequal("numero", numeroCuenta));
+                Objects<Cuentas> cuentasObjects = odb.getObjects(criteria);
+
+                cuentasObjects.getFirst().setSaldo(cuentasObjects.getFirst().getSaldo() + Float.parseFloat(txtSaldoIng.getText()));
+
+                odb.store(cuentasObjects.getFirst());
+                odb.commit();
+                odb.close();
+            }
         } else {
             lblAdvertencia2.setText(mensaje);
         }
@@ -258,91 +321,152 @@ public class ControladorBanco implements Initializable {
 
     public void seleccionAumentoSaldo() {
         txtSaldoIng.setText("");
-        Session s = NewHibernateUtil.getSession();
-        Clientes cli = (Clientes) s.get(Clientes.class, LoginTemp.getClienteActual().getId());
+        if (LoginTemp.bbdd == 0) {
+            Session s = NewHibernateUtil.getSession();
+            Clientes cli = (Clientes) s.get(Clientes.class, LoginTemp.getClienteActual().getId());
 
-        for (Cuentas c : cli.getCuentas()) {
-            if (c.getNumeroCuenta().equals(cmbCuenta2.getSelectionModel().getSelectedItem())) {
-                lblSaldo.setText(String.valueOf(c.getSaldo()));
+            for (Cuentas c : cli.getCuentas()) {
+                if (c.getNumeroCuenta().equals(cmbCuenta2.getSelectionModel().getSelectedItem())) {
+                    lblSaldo.setText(String.valueOf(c.getSaldo()));
+                }
             }
+            s.close();
+        } else {
+            ODB odb = ODBFactory.openClient("localhost", 8000, "proyectojjcv");
+            Clientes cliente = ODBCliente(odb);
+
+            for (Cuentas c : cliente.getCuentas()) {
+                if (c.getNumeroCuenta().equals(cmbCuenta2.getSelectionModel().getSelectedItem())) {
+                    lblSaldo.setText(String.valueOf(c.getSaldo()));
+                }
+            }
+            odb.close();
         }
-        s.close();
     }
 
     public void confAumentoSaldo() {
-        Session s = NewHibernateUtil.getSession();
-        Clientes cli = (Clientes) s.get(Clientes.class, LoginTemp.getClienteActual().getId());
+        if (LoginTemp.bbdd == 0) {
+            Session s = NewHibernateUtil.getSession();
+            Clientes cli = (Clientes) s.get(Clientes.class, LoginTemp.getClienteActual().getId());
 
-        for (Cuentas c : cli.getCuentas()) {
-            if (c.getNumeroCuenta().equals(cmbCuenta2.getSelectionModel().getSelectedItem())) {
-                c.setSaldo(Float.parseFloat(lblSaldo.getText()) + Float.parseFloat(txtSaldoIng.getText()));
-                lblSaldo.setText(String.valueOf(c.getSaldo()));
-                s.beginTransaction();
-                s.saveOrUpdate(c);
-                s.getTransaction().commit();
+            for (Cuentas c : cli.getCuentas()) {
+                if (c.getNumeroCuenta().equals(cmbCuenta2.getSelectionModel().getSelectedItem())) {
+                    c.setSaldo(Float.parseFloat(lblSaldo.getText()) + Float.parseFloat(txtSaldoIng.getText()));
+                    lblSaldo.setText(String.valueOf(c.getSaldo()));
+                    s.beginTransaction();
+                    s.saveOrUpdate(c);
+                    s.getTransaction().commit();
+                }
             }
+            txtSaldoIng.setText("");
+            s.close();
+        } else {
+            ODB odb = ODBFactory.openClient("localhost", 8000, "proyectojjcv");
+            Clientes cliente = ODBCliente(odb);
+            for (Cuentas c : cliente.getCuentas()) {
+                if (c.getNumeroCuenta().equals(cmbCuenta2.getSelectionModel().getSelectedItem())) {
+                    c.setSaldo(Float.parseFloat(lblSaldo.getText()) + Float.parseFloat(txtSaldoIng.getText()));
+                    lblSaldo.setText(String.valueOf(c.getSaldo()));
+                    odb.store(cliente);
+                    odb.commit();
+
+                }
+            }
+            txtSaldoIng.setText("");
+            odb.close();
         }
-        txtSaldoIng.setText("");
-        s.close();
     }
 
     public void datosBanco() {
-        Session s = NewHibernateUtil.getSession();
-        Clientes cli = (Clientes) s.get(Clientes.class, LoginTemp.getClienteActual().getId());
+        if (LoginTemp.bbdd == 0) {
+            Session s = NewHibernateUtil.getSession();
+            Clientes cli = (Clientes) s.get(Clientes.class, LoginTemp.getClienteActual().getId());
 
+            cargarDatos(cli);
+
+            s.close();
+        } else {
+            ODB odb = ODBFactory.openClient("localhost", 8000, "proyectojjcv");
+            Clientes cliente = ODBCliente(odb);
+            cargarDatos(cliente);
+            odb.close();
+        }
+    }
+
+    private void cargarDatos(Clientes cli) {  // Metodo creado para cargar el ciente actual de datosBanco()
         for (Cuentas c : cli.getCuentas()) {
             if (c.getNumeroCuenta().equals(cmbCuenta.getSelectionModel().getSelectedItem())) {
                 SimpleDateFormat sdf = new SimpleDateFormat("MM/yyyy");
-                txtTitular.setText(c.getCliente().getNombre());//El onjeto cuenta no tiene titular 
+                txtTitular.setText(c.getCliente().getNombre());
                 txtTarjeta.setText(c.getTarjeta().getNumeroTarjeta());
                 txtSaldo.setText(String.valueOf(c.getSaldo()));
                 txtCVV.setText(String.valueOf(c.getTarjeta().getCvv()));
                 txtCaducidad.setText(sdf.format(c.getTarjeta().getFechaCaducidad()));
-
             }
         }
-        s.close();
     }
+
 
     public void eliminarCuenta() {
         byte b = 0;
         Cuentas cu = null;
         Tarjetas t = null;
-        Session s = NewHibernateUtil.getSession();
-        Clientes cli = (Clientes) s.get(Clientes.class, LoginTemp.getClienteActual().getId());
 
-        for (Cuentas c : cli.getCuentas()) {
-            if (c.getNumeroCuenta().equals(cmbCuenta.getSelectionModel().getSelectedItem())) {
-                Windows win = new Windows();
-                b = win.dialogo("¿Desea eliminar esta cuenta?");
-                cu = c;
-                t = c.getTarjeta();
+        if (LoginTemp.bbdd == 0) {
+            Session s = NewHibernateUtil.getSession();
+            Clientes cli = (Clientes) s.get(Clientes.class, LoginTemp.getClienteActual().getId());
 
+            for (Cuentas c : cli.getCuentas()) {
+                if (c.getNumeroCuenta().equals(cmbCuenta.getSelectionModel().getSelectedItem())) {
+                    Windows win = new Windows();
+                    b = win.dialogo("¿Desea eliminar esta cuenta?");
+                    cu = c;
+                    t = c.getTarjeta();
+
+                }
             }
-        }
-        if (b == -1) {
-            cli.getCuentas().remove(cu);
-            txtTitular.setText("");//El objeto cuenta no tiene titular 
-            txtTarjeta.setText("");
-            txtSaldo.setText("");
-            txtCVV.setText("");
-            txtCaducidad.setText("");
-            s.beginTransaction();
-            s.delete(cu);
-            s.delete(t);
-            s.getTransaction().commit();
-            cmbCuenta.getItems().clear();
-            cmbCuenta2.getItems().clear();
-            Clientes clie = (Clientes) s.get(Clientes.class, LoginTemp.getClienteActual().getId());
-            for (Cuentas cue : clie.getCuentas()) {
-                cmbCuenta.getItems().add(cue.getNumeroCuenta());
-                cmbCuenta2.getItems().add(cue.getNumeroCuenta());
+            if (b == -1) {
+                cli.getCuentas().remove(cu);
+                vaciar();
+                s.beginTransaction();
+                s.delete(cu);
+                s.delete(t);
+                s.getTransaction().commit();
+                cmbCuenta.getItems().clear();
+                cmbCuenta2.getItems().clear();
+                Clientes clie = (Clientes) s.get(Clientes.class, LoginTemp.getClienteActual().getId());
+                cargarCombos(clie);
+                cmbCuenta.getSelectionModel().select(-1);
             }
-            cmbCuenta.getSelectionModel().select(-1);
 
+            s.close();
+        } else {
+            ODB odb = ODBFactory.openClient("localhost", 8000, "proyectojjcv");
+            Clientes cliente = ODBCliente(odb);
+
+            for (Cuentas c : cliente.getCuentas()) {
+                if (c.getNumeroCuenta().equals(cmbCuenta.getSelectionModel().getSelectedItem())) {
+                    Windows win = new Windows();
+                    b = win.dialogo("¿Desea eliminar esta cuenta?");
+                    cu = c;
+                    t = c.getTarjeta();
+
+                }
+            }
+            if (b == -1) {
+                cliente.getCuentas().remove(cu);
+                vaciar();
+                odb.delete(cu);
+                odb.delete(t);
+                odb.commit();
+                cmbCuenta.getItems().clear();
+                cmbCuenta2.getItems().clear();
+                cargarCombos(cliente);
+                cmbCuenta.getSelectionModel().select(-1);
+            }
+
+            odb.close();
         }
-
-        s.close();
     }
 
 }
